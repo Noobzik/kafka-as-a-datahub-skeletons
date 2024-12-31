070e69ca09be73e7c22bb0dcf7f18b03d2b2545c
NoobZik <noobzik@pm.me>
2024-12-31 17:13:53 +0100

    Reverted StreamProcessing template

--- kafka-stream/src/main/scala/org/esgi/project/streaming/StreamProcessing.scala
+++ kafka-stream/src/main/scala/org/esgi/project/streaming/StreamProcessing.scala
@@ -4,8 +4,10 @@ import io.github.azhur.kafka.serde.PlayJsonSupport
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.streams.scala._
-import org.apache.kafka.streams.scala.kstream.{KTable, Materialized}
+import org.apache.kafka.streams.scala.kstream._
 import org.apache.kafka.streams.{KafkaStreams, StreamsConfig}
+import org.apache.kafka.streams.kstream.Windowed
+import org.esgi.project.streaming.models.{MeanLatencyForURL, Metric, Visit, VisitWithLatency}
 
 import java.util.Properties
 
@@ -14,22 +16,78 @@ object StreamProcessing extends PlayJsonSupport {
   import org.apache.kafka.streams.scala.ImplicitConversions._
   import org.apache.kafka.streams.scala.serialization.Serdes._
 
-  val applicationName = s"some-application-name"
+  // TODO: Predeclared store names to be used, fill your first & last name
+  val yourFirstName: String = ???
+  val yourLastName: String = ???
 
-  private val props: Properties = buildProperties
+  val applicationName = s"web-events-stream-app-$yourFirstName-$yourLastName"
+
+  val visitsTopicName: String = "visits"
+  val metricsTopicName: String = "metrics"
+
+  val thirtySecondsStoreName: String = "VisitsOfLast30Seconds"
+  val lastMinuteStoreName = "VisitsOfLastMinute"
+  val lastFiveMinutesStoreName = "VisitsOfLast5Minutes"
+
+  val thirtySecondsByCategoryStoreName: String = "VisitsOfLast30SecondsByCategory"
+  val lastMinuteByCategoryStoreName = "VisitsOfLastMinuteByCategory"
+  val lastFiveMinutesByCategoryStoreName = "VisitsOfLast5MinutesByCategory"
+  val meanLatencyForURLStoreName = "MeanLatencyForURL"
+
+  val props = buildProperties
 
   // defining processing graph
   val builder: StreamsBuilder = new StreamsBuilder
 
-  val wordTopic = "words"
-  val wordCountStoreName = "word-count-store"
+  // TODO: declared topic sources to be used
+  val visits: KStream[String, Visit] = builder.stream[String, Visit](visitsTopicName)
+  val metrics: KStream[String, Metric] = builder.stream[String, Metric](metricsTopicName)
 
-  val words = builder.stream[String, String](wordTopic)
+  /**
+   * -------------------
+   * Part.1 of exercise
+   * -------------------
+   */
+  // TODO: repartition visits per URL
+  val visitsGroupedByUrl: KGroupedStream[String, Visit] = ???
 
-  val wordCounts: KTable[String, Long] = words
-    .flatMapValues(textLine => textLine.toLowerCase.split("\\W+"))
-    .groupBy((_, word) => word)
-    .count()(Materialized.as(wordCountStoreName))
+  // TODO: implement a computation of the visits count per URL for the last 30 seconds,
+  // TODO: the last minute and the last 5 minutes
+  val visitsOfLast30Seconds: KTable[Windowed[String], Long] = ???
+
+  val visitsOfLast1Minute: KTable[Windowed[String], Long] = ???
+
+  val visitsOfLast5Minute: KTable[Windowed[String], Long] = ???
+
+  /**
+   * -------------------
+   * Part.2 of exercise
+   * -------------------
+   */
+  // TODO: repartition visits topic per category instead (based on the 2nd part of the URLs)
+  val visitsGroupedByCategory: KGroupedStream[String, Visit] = ???
+
+  // TODO: implement a computation of the visits count per category for the last 30 seconds,
+  // TODO: the last minute and the last 5 minutes
+  val visitsOfLast30SecondsByCategory: KTable[Windowed[String], Long] = ???
+
+  val visitsOfLast1MinuteByCategory: KTable[Windowed[String], Long] = ???
+
+  val visitsOfLast5MinuteByCategory: KTable[Windowed[String], Long] = ???
+
+  // TODO: implement a join between the visits topic and the metrics topic,
+  // TODO: knowing the key for correlated events is currently the same UUID (and the same id field).
+  // TODO: the join should be done knowing the correlated events are emitted within a 5 seconds latency.
+  // TODO: the outputted message should be a VisitWithLatency object.
+  val visitsWithMetrics: KStream[String, VisitWithLatency] = ???
+
+  // TODO: based on the previous join, compute the mean latency per URL
+  val meanLatencyPerUrl: KTable[String, MeanLatencyForURL] = ???
+
+  // -------------------------------------------------------------
+  // TODO: now that you're here, materialize all of those KTables
+  // TODO: to stores to be able to query them in Webserver.scala
+  // -------------------------------------------------------------
 
   def run(): KafkaStreams = {
     val streams: KafkaStreams = new KafkaStreams(builder.build(), props)
@@ -50,6 +108,8 @@ object StreamProcessing extends PlayJsonSupport {
     properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     properties.put(StreamsConfig.CLIENT_ID_CONFIG, applicationName)
     properties.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationName)
+    // Disable caching to print the aggregation value after each record
+    // properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0")
     properties.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, "0")
     properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
     properties.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, "-1")
